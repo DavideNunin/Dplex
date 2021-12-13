059d7a543b5bda48e449a22b12288cc44ba85c9c
Vito Scaraggi <s1092648@studenti.univpm.it>
2021-12-13 15:55:26 +0100

    fix solve.cpp

--- src/solve.cpp
+++ src/solve.cpp
@@ -6,16 +6,24 @@ using namespace std;
 
 #include "header.h"
 
+bool is_prob_wellformed(problem p){
+	return p.rows < p.cols;
+}
+
+
 void solve(problem p){
 	print_problem(p);
-	tableau t = prob_to_tab(p);
-	print_tableau(t);
-    t = phase1(t, p);
-	if (!is_prob_empty(t)){
-		cout<<"Eseguendo fase 2 su problema originale...\n\n";
-		t = phase2(t,p.dir);
+	if(is_prob_wellformed(p)){
+		tableau t = prob_to_tab(p);
 		print_tableau(t);
-		if(!is_prob_unbounded(t, p.dir))
-			cout<<"La soluzione ottima è "<<-t.tab[0][t.cols-1]<<"\n";
+    	t = phase1(t, p);
+		if (!is_prob_empty(t)){
+			cout<<"Eseguendo fase 2 su problema originale...\n\n";
+			t = phase2(t,p.dir);
+			print_tableau(t);
+			if(!is_prob_unbounded(t, p.dir))
+				cout<<"La soluzione ottima è "<<-t.tab[0][t.cols-1]<<"\n";
+		}
 	}
+	else cout<<"Problema malformato\n";
 }
